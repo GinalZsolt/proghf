d1893cf339c05b58dcda6698db3cbbee468e8cfc
GinalZsolt <ginalzsolt@gmail.com>
2021-11-04 10:01:13 +0100

    háttérjavítás

--- dinosugralosjacci/Properties/Resources.Designer.cs
+++ dinosugralosjacci/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace dinosugralosjacci.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hatter {
+            get {
+                object obj = ResourceManager.GetObject("hatter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap jobbdino {
             get {
                 object obj = ResourceManager.GetObject("jobbdino", resourceCulture);
